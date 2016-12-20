09a84cc672df92e577df5a873bc7ea199394556e
Robin Buckley <robin.buckley@auspost.com.au>
2016-12-20 18:03:18 +1100

    use hashHistory for gh pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, Link, IndexRoute, browserHistory} from 'react-router';
+import {Router, Route, Link, IndexRoute, browserHistory, hashHistory} from 'react-router';
 
 import App from './App';
 import Home from './components/home/Home';
@@ -9,9 +9,10 @@ import './index.css';
 console.log('starting...');
 
 ReactDOM.render(
-  <Router history={browserHistory} >
+  <Router history={hashHistory} >
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
+      <Route path='/home' component={Home} />
     </Route>
   </Router>,
   document.getElementById('root')
