d3e0aa2da67c2d77e19069eec21844fbddd9e229
Robin Buckley <robin.buckley@auspost.com.au>
2016-12-20 16:18:34 +1100

    Github pages task

---
+++ scripts/gh-deploy.js
@@ -0,0 +1,45 @@
+var ghpages = require('gh-pages');
+var path = require('path');
+var jsdom = require("jsdom");
+var fs = require("fs");
+
+var filePath = path.join(__dirname, '../build/index.html');
+
+function setBaseURL(baseUrl, callback) {
+
+  fs.readFile(filePath, 'utf8', function(error, data) {
+
+      jsdom.env(data, [], function (errors, window) {
+          var base = window.document.querySelector('base');
+          base.setAttribute('href', baseUrl);
+
+          fs.writeFile(
+            filePath, 
+            window.document.documentElement.outerHTML,
+            function (error){
+              if (!error) {
+                callback();
+              } else {
+                throw error 
+              };
+            }
+          );
+
+      });
+  });
+}
+//'amu-react-webaudio-spike/'
+
+setBaseURL('/amu-react-webaudio-spike', () => {
+  ghpages.publish(path.join(__dirname, '../build'), function(err) {
+    console.log('Attempting to deploy \'./build\' to Github Pages');
+    if (!err) {
+      setBaseURL('/', function () {
+        console.log('index.html restored');
+      });
+      console.log('Deployed!');
+    } else {
+      console.log(err);
+    }
+  });
+});
